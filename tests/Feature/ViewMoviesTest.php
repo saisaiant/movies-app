b4ee7bd46b714bf6e73d5ce8ee1c7d00421b3008
Sai Aung Naing Tun <saisaiant@gmail.com>
2020-04-09 14:19:42 +0630

    add livewire search component

---
+++ tests/Feature/ViewMoviesTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ViewMoviesTest extends TestCase
+{
+    // public function the_main_page_shows_correct_info()
+    // {
+
+    // }
+}
