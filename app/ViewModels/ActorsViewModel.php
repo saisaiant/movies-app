f36e9695888598ec6766b61652a65283f46674a3
Sai Aung Naing Tun <saisaiant@gmail.com>
2020-04-10 14:23:00 +0630

    add actor section

--- app/ViewModels/ActorsViewModel.php
+++ app/ViewModels/ActorsViewModel.php
@@ -29,7 +29,7 @@ class ActorsViewModel extends ViewModel
 
             ])->only([
                 'name', 'id', 'profile_path', 'known_for'
-            ])->dump();
+            ]);
         });
     }
 
